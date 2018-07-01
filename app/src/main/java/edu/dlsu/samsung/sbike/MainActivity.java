56be507a05e867a95d6e24b71c74ee394f621fd3
Jan Bertel Ngo <jan_bertel_ngo@dlsu.edu.ph>
2018-07-01 19:38:40 +0800

    Initial Commit

---
+++ app/src/main/java/edu/dlsu/samsung/sbike/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.dlsu.samsung.sbike;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
